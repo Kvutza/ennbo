bcf1f0a3e218a36d0123a3c15883b9f436e687e7
David Sweet <dsweet@andamooka.org>
2025-11-22 09:54:20 -0500

    ok

--- src/enn/__init__.py
+++ src/enn/__init__.py
@@ -1,1 +1,10 @@
-__all__: list[str] = []
+from .core import EpistemicNearestNeighbors
+from .enn_normal import ENNNormal
+from .fit import enn_fit, subsample_loglik
+
+__all__: list[str] = [
+    "EpistemicNearestNeighbors",
+    "ENNNormal",
+    "enn_fit",
+    "subsample_loglik",
+]
